499b0905fa934d85edd1879f036a6968fc51d815
hetoney <toneyhe@yahoo.ca>
2021-08-04 20:25:04 -0400

    <base href="/app/"> removed

--- systemjs.config.js
+++ systemjs.config.js
@@ -27,6 +27,7 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
+      'rxjs/operators': 'npm:rxjs/operators/index.js',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
